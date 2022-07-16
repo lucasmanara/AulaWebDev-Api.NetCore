ca9e66abb5c028dec059a57b6c81c9018e235122
Lucas <lucaasmaanaaraa@gmail.com>
2022-07-16 15:41:47 -0300

    config do banco de dados

---
+++ AulaWebDev.Infra/Dependencias/InjecaoDependenciaExtension.cs
@@ -0,0 +1,22 @@
+﻿using AulaWebDev.Infra.Context;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaWebDev.Infra.Dependencias
+{
+    public static class InjecaoDependenciaExtension
+    {
+        public static IServiceCollection AddInfraestrutura(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("SqliteConnectionString");
+            services.AddSqlite<AulaWebDevDbContext>(connectionString);
+
+            return services;
+        }
+    }
+}
