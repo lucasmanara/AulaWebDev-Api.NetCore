6930ae8ff407c5f44c5ae34e06bb0bc5baa05feb
Lucas <lucaasmaanaaraa@gmail.com>
2022-07-16 15:13:23 -0300

    mapeamento de cliente

---
+++ AulaWebDev.Dominio/Entidades/Produto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaWebDev.Dominio.Entidades
+{
+    public class Produto
+    {
+        public Guid Id { get; private set; }
+        public string Descricao { get; private set; }
+        public int Codigo { get; set; }
+        public decimal Valor { get; set; }
+        public int QuantidadeEstoque { get; set; }
+        public ICollection<Pedido> Pedidos { get; set; }
+
+        public Produto(Guid id, string descricao, int codigo, decimal valor, int quantidadeEstoque)
+        {
+            Id = id;
+            Descricao = descricao;
+            Codigo = codigo;
+            Valor = valor;
+            QuantidadeEstoque = quantidadeEstoque;
+        }
+
+        public Produto(string descricao, int codigo, decimal valor, int quantidadeEstoque)
+        {
+            Descricao = descricao;
+            Codigo = codigo;
+            Valor = valor;
+            QuantidadeEstoque = quantidadeEstoque;
+        }
+    }
+}
---
+++ AulaWebDev.Infra/Context/AulaWebDevDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaWebDev.Infra.Context
+{
+    public class AulaWebDevDbContext : DbContext
+    {
+        public AulaWebDevDbContext(DbContextOptions<AulaWebDevDbContext> options) : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AulaWebDevDbContext).Assembly);
+        }
+    }
+}
