4cdfa0bb20bf6d3e51521f5a0097a13607a0b6ba
Lucas <lucaasmaanaaraa@gmail.com>
2022-07-16 17:25:25 -0300

    configurando controller de cliente

---
+++ AulaWebDev.API/ClientesController.cs
@@ -0,0 +1,30 @@
+﻿using AulaWebDev.Aplicacao.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AulaWebDev.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        private readonly IClienteService _clienteService;
+
+        public ClientesController(IClienteService clienteService)
+        {
+            _clienteService = clienteService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            var result = await _clienteService.ObterTodosAsync();
+            if (result.Sucesso)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+    }
+}
--- AulaWebDev.API/Program.cs
+++ AulaWebDev.API/Program.cs
@@ -1,4 +1,5 @@
 using AulaWebDev.Infra.Dependencias;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,8 +9,25 @@ builder.Services.AddLogging();
 builder.Services.AddInfraestrutura(builder.Configuration);
 builder.Services.AddServices();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "WebDevAlfa",
+        Description = "Api criada para aula de pós graduação da faculdade Alfa",
+        Version = "v1"
+    });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API de produto v1");
+});
 
 app.Run();
